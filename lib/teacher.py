bea3fa519df1a7450c58266751119b114c810b31
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-08-23 14:19:36 -0400

    Assignment complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
-
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[index]
